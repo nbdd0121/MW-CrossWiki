894ace32f07e6dbf255852c22b35de0c0d8a0ea8
Gary Guo <nbdd0121@hotmail.com>
2016-06-07 08:17:30 -0700

    Make UI better

--- includes/SpecialCrossWiki.php
+++ includes/SpecialCrossWiki.php
@@ -16,14 +16,34 @@ class SpecialCrossWiki extends \UnlistedSpecialPage {
         $this->outputHeader();
         $output = $this->getOutput();
 
+        $out = '';
+        $out .= \Html::openElement('table', ['class' => 'wikitable']);
+        $out .= \Html::openElement('tr');
+        $out .= \Html::element('th', [], $this->msg('crosswiki_title_name')->text());
+        $out .= \Html::element('th', [], $this->msg('crosswiki_title_interwiki')->text());
+        $out .= \Html::element('th', [], $this->msg('crosswiki_title_entrypoint')->text());
+        $out .= \Html::element('th', [], $this->msg('crosswiki_title_status')->text());
+        $out .= \Html::closeElement('tr');
+
         foreach ($wgCrossWiki as $host => $val) {
+            $out .= \Html::openElement('tr');
+            $out .= \Html::element('td', [], $val['name']);
+            $out .= \Html::element('td', [], $val['interwiki']);
+            $out .= \Html::element('td', [], $val['entrypoint']);
             try {
                 $resp = \CrossWiki::send(['type' => 'ping'], $host);
-                $output->addWikiMsg('crosswiki_success', $val['name'], $host);
+                $out .= \Html::element('td', ['style' => 'background-color: #afc;'],
+                    $this->msg('crosswiki_success', $val['name'], $host)->text());
             } catch (\Exception $ex) {
-                $output->addWikiMsg('crosswiki_failure', $val['name'], $host, $ex->getMessage());
+                $out .= \Html::element('td', ['style' => 'background: pink;'],
+                    $this->msg('crosswiki_failure', $val['name'], $host, $ex->getMessage())->text());
             }
+            $out .= \Html::closeElement('tr');
         }
+
+        $out .= \Html::closeElement('table');
+
+        $output->addHTML($out);
 
     }
 
